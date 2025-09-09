46564922dfa17ce9f4cdd89ed55b18009de0a50b
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-09 18:46:50 +0300

    js DOM

--- DOM.js
+++ DOM.js
@@ -13,7 +13,38 @@ let btn3 = document.querySelector('#btn_3');
 
 // 1. Elementu atlase no dokumenta pēc CSS selektora (atlasa vairākus uzreiz)
 
-let btns = document.querySelector('button');
+let btns = document.querySelectorAll('button');
 
 console.log(virsraksts1,btns);
 
+
+// Vecās atlases metodes
+
+// document.getElementById('')
+// document.getElementsByName('')
+// document.getElementsByClassName('')
+
+// let oldMethod = document.getElementsByTagName('div');
+
+// console.log(oldMethod)
+
+
+// 2. Notikumi
+
+virsraksts1.onclick = function(){
+    document.body.style.background = "#be1212ff";
+}
+virsraksts1.onmouseenter = function(){
+    virsraksts1.style.color = "#0d41deff";
+}
+
+virsraksts1.onmouseleave = function(){
+    virsraksts1.style.color = "#11de0dff";
+}
+
+document.body.onkeyup = function(event){
+    console.log(event)
+    if(event.key=='Escape'){
+         document.body.style.background = "none";
+    }
+}
