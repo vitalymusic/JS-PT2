3e25ce50025027c5720a13f817420067ad777f35
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-09 18:21:35 +0300

    DOM

---
+++ DOM.js
@@ -0,0 +1,19 @@
+// DOM 
+
+
+// 1. Elementu atlase no dokumenta pēc CSS selektora
+
+let virsraksts1 = document.querySelector('h1');
+let virsraksts2 = document.querySelector('h2');
+let rindkopa = document.querySelector('.content');
+
+let btn1 = document.querySelector('#btn_1');
+let btn2 = document.querySelector('#btn_2');
+let btn3 = document.querySelector('#btn_3');
+
+// 1. Elementu atlase no dokumenta pēc CSS selektora (atlasa vairākus uzreiz)
+
+let btns = document.querySelector('button');
+
+console.log(virsraksts1,btns);
+
